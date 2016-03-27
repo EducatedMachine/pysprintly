0b68e556a0dcfe7d385ef28fdacfd367eac6cf58
Micheal Reilly <info@educatedmachine.com>
2016-03-27 04:54:59 +0100

    Added beginning of simple caching

--- api/products.py
+++ api/products.py
@@ -3,6 +3,7 @@ class Products:
     def __init__(self, connection, cache_values=True):
         self.conn = connection
         self.cache_values = cache_values
+        self.products = {}
 
     @property
     def all_products(self):
@@ -12,7 +13,10 @@ class Products:
         return self.conn.post("PRODUCTS", data={"name": product_name})
 
     def get_product(self, product_id):
-        return self.conn.get("PRODUCT", fields=product_id)
+        if self.cache_values and product_id not in self.products:
+            return self.conn.get("PRODUCT", fields=product_id)
+        else:
+            return self.conn.get("PRODUCT", fields=product_id)
 
     def update_product(self):
         pass
