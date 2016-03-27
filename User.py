23aa80faa5dbe94265d0028deca43c39ad05474d
Micheal Reilly <info@educatedmachine.com>
2016-03-27 03:12:45 +0100

    Add and reformatted list of api endpoints

--- User.py
+++ User.py
@@ -1,4 +1,45 @@
 class User:
 
-    def __init__(self, user_json):
-        self.user_json = user_json
+    def __init__(self, connection, cache_values=True):
+        self.conn = connection
+        self.user = ""
+        self.cache_values = cache_values
+
+    @property
+    def user_id(self):
+        return self.get_field("id")
+
+    @property
+    def first_name(self):
+        return self.get_field("first_name")
+
+    @property
+    def last_name(self):
+        return self.get_field("last_name")
+
+    @property
+    def created_at(self):
+        return self.get_field("created_at")
+
+    @property
+    def last_login(self):
+        return self.get_field("last_login")
+
+    @property
+    def user_details(self):
+        return self.user or self.conn.fetch("USER")
+
+    def get_field(self, field_name):
+        if self.cache_values:
+            r = self.user or self.conn.fetch("USER")
+        else:
+            r = self.conn.fetch("USER")
+
+        if not r[0]:
+            return r[1]
+
+        try:
+            value = r[1]
+            return value[field_name]
+        except KeyError:
+            return "Key error could not find {}".format(field_name)
--- sprintly.py
+++ sprintly.py
@@ -4,25 +4,49 @@ import products
 from requests.auth import HTTPBasicAuth
 
 
-api_key = "4RZbU9XN7ZFcLdmfdqWA7rJBNELGrc5K"
-username = "info@educatedmachine.com"
 
 #   =====================================
 #
-#   BASE URLS
+#   API ENDPOINT URLS
 #
 #   =====================================
-
-BASE = "https://sprint.ly/api/"
 
 URLS = {
-    "BASE": BASE,
-    "USER": BASE + "user/",
-    "PRODUCTS": BASE + "products/",
+    "BASE_API_ENDPOINT": "https://sprint.ly/api/",
+    "USER": "user/whoami.json",
+    "PRODUCTS": "products/",
+
+    "ITEMS": "products/{product_id}/items.json",
+    "ITEM": "products/{product_id}/items/{item_number}.json",
+    "ITEM_CHILDREN": "products/{product_id}/items/{item_number}/children.json",
+
+    "TAGS": "products/{product_id}/tags.json",
+    "TAG": "products/{product_id}/tags/{tag_name}.json",
+
+    "ATTACHMENTS": "products/{product_id}/items/{item_number}/attachments.json",
+    "ATTACHMENT": "products/{product_id}/items/{item_number}/attachments/{attachment_id}.json",
+
+    "BLOCKS": "products/{product_id}/items/{item_number}/blocking.json",
+    "BLOCK": "products/{product_id}/items/{item_number}/blocking/{block_id}.json",
+
+    "COMMENTS": "products/{product_id}/items/{item_number}/comments.json",
+    "COMMENT": "products/{product_id}/items/{item_number}/comments/{comment_id}.json",
+
+    "DEPLOYS": "products/{product_id}/deploys.json",
+
+    "FAVOURITES": "products/{product_id}/items/{item_number}/favorites.json",
+    "FAVOURITE": "products/{product_id}/items/{item_number}/favorites/{favorite_id}.json",
+
+    "PEOPLE": "products/{product_id}/people.json",
+    "PERSON": "products/{product_id}/people/{user_id}.json",
+
+    "PULL_REQUESTS": "products/{product_id}/pullrequests.json",
+    "PULL_REQUEST": "products/{product_id}/pullrequests/{pull_request_number}.json",
 }
 
 
 class Connection:
+
     def __init__(self, email, api_key, cache_values=True):
         self.email = email
         self.api_key = api_key
@@ -31,8 +55,24 @@ class Connection:
         self.user = ""
         self.products = products.Products(self)
 
-    def fetch(self, endpoint):
+    def get(self, URL):
+        endpoint = self.URLS[URL]
+        r = requests.get(endpoint, auth=self.auth)
+        try:
+            value = json.loads(r.text)
+            self.user = (True, value)
+            return True, value
+        except TypeError:
+            return False, "Error returning value, incorrect JSON format"
+
+    def post(self, URL, data):
         pass
+
+    def delete(self, URL, data):
+        pass
+
+    def build_url(self, URL, fields):
+        return self.URLS[URL]
 
     @property
     def username(self):
@@ -42,48 +82,3 @@ class Connection:
     def auth(self):
         return HTTPBasicAuth(self.email, self.api_key)
 
-    @property
-    def user_id(self):
-        return self.get_field("id")
-
-    @property
-    def first_name(self):
-        return self.get_field("first_name")
-
-    @property
-    def last_name(self):
-        return self.get_field("last_name")
-
-    @property
-    def created_at(self):
-        return self.get_field("created_at")
-
-    @property
-    def last_login(self):
-        return self.get_field("last_login")
-
-    @property
-    def user_details(self):
-        endpoint = self.URLS["USER"] + "whoami.json"
-        r = requests.get(endpoint, auth=self.auth)
-        try:
-            value = json.loads(r.text)
-            self.user = (True, value)
-            return True, value
-        except TypeError:
-            return False, "Error returning value, incorrect JSON format"
-
-    def get_field(self, field_name):
-        if self.cache_values:
-            r = self.user or self.user_details
-        else:
-            r = self.user_details
-
-        if not r[0]:
-            return r[1]
-
-        try:
-            value = r[1]
-            return value[field_name]
-        except KeyError:
-            return "Key error could not find {}".format(field_name)
